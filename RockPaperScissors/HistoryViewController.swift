27c4f546883f53eb46e4705a3f4241f3b7a9e1d3
Alaa Alali <aalali04@gmail.com>
2019-09-12 15:20:01 +0300

    implementing the table functions

--- RockPaperScissors/HistoryViewController.swift
+++ RockPaperScissors/HistoryViewController.swift
@@ -12,18 +12,42 @@ import UIKit
 // MARK: - HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 
 class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return history.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
     
     
     // MARK: Properties
     
     var history: [RPSMatch]!
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return history.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "historyID")!
+        let match = self.history[(indexPath as NSIndexPath).row]
+        cell.textLabel!.text = matchresult(match)
+        cell.detailTextLabel!.text = "\(match.p1) vs. \(match.p2)"
+
+        
+        return cell
+    }
+
+    
+    // MARK: Image for Match
+    
+    func matchresult(_ match: RPSMatch) -> String {
+        
+        var result = ""
+        
+        if match.p1 == match.p2 {
+            result = "itsATie"
+        } else if (match.p1.defeats(match.p2)) {
+            result = "Win!"
+        } else {
+            result = "Loss"
+        }
+        return result
+    }
 
 }
