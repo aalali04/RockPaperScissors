91ee5a0944f282e1cd0c9967f6ef80a78975f409
Alaa Alali <aalali04@gmail.com>
2019-09-12 11:16:22 +0300

    add the HistoryViewController

---
+++ RockPaperScissors/HistoryViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HistoryViewController.swift
+//  RockPaperScissors
+//
+//  Created by Alaa Alali on 12/09/2019.
+//  Copyright © 2019 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
